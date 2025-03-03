713f302f9fb0a885a1c98c4b8b52131ad48de761
Al Dm <d44.a1@yandex.ru>
2025-03-03 21:58:53 +0300

    1

--- js/index.js
+++ js/index.js
@@ -1,26 +1,3 @@
-const moreButton = document.querySelector(".in-demand__more");
-const preItems = document.querySelector(".in-demand__grid-pre");
-const mainItems = document.querySelector(".in-demand__swiper");
-const backButton = document.querySelector(".in-demand__back");
-
-if (moreButton) {
-  moreButton.addEventListener("click", () => {
-    preItems.classList.add("active");
-    mainItems.classList.add("active");
-    moreButton.classList.add("active");
-    backButton.classList.add("active");
-  });
-}
-
-if (backButton) {
-  backButton.addEventListener("click", () => {
-    preItems.classList.remove("active");
-    mainItems.classList.remove("active");
-    moreButton.classList.remove("active");
-    backButton.classList.remove("active");
-  });
-}
-
 //мобильное меню
 const burgerBtn = document.querySelector(".header__burger");
 const bodyElement = document.querySelector(".body");
---
+++ js/init-sought-after.js
@@ -0,0 +1,50 @@
+const initSoughtAfter = [
+  {
+    image: "../../images/warfacebezss.png",
+    name: "Аккаунты для warface",
+  },
+  {
+    image: "../../images/warfacebezss.png",
+    name: "Аккаунты для ПОРНОХАБ",
+  },
+  {
+    image: "../../images/warfacebezss.png",
+    name: "Аккаунты для ГАЧИ-МУЧИ",
+  },
+];
+
+//инициализация карточек каталог
+function setEventListeners(htmlElement) {}
+const itemTemplate = document.getElementById("sought-after").content;
+const elements = document.querySelector(".ac-elements");
+
+function createCard(item) {
+  const htmlElement = itemTemplate.cloneNode(true);
+  const acImage = htmlElement.querySelector(".pre__image");
+  const acName = htmlElement.querySelector(".in-demand__text h3");
+
+  // Проверяем наличие значения поля в объекте item перед присваиванием его элементу
+
+  if (item.image) {
+    acImage.src = item.image;
+  }
+  if (item.name) {
+    acName.textContent = item.name;
+  }
+  setEventListeners(htmlElement);
+
+  return htmlElement;
+}
+
+initSoughtAfter.forEach(function (item) {
+  const elementCreate = createCard(item);
+  elements.append(elementCreate);
+});
+
+const moreButton = document.querySelector(".in-demand__more");
+
+if (initSoughtAfter.length > 3) {
+  console.log(111);
+} else {
+  console.log(222);
+}
