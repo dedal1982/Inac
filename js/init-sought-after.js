babc486d0f033c8e9525c6fd29affa76772df7dd
Al Dm <d44.a1@yandex.ru>
2025-03-03 23:00:29 +0300

    1

--- js/init-sought-after.js
+++ js/init-sought-after.js
@@ -1,14 +1,14 @@
 const initSoughtAfter = [
   {
-    image: "../../images/warfacebezss.png",
+    image: "../images/warfacebezss.png",
     name: "Аккаунты для warface",
   },
   {
-    image: "../../images/warfacebezss.png",
+    image: "../images/warfacebezss.png",
     name: "Аккаунты для ПОРНОХАБ",
   },
   {
-    image: "../../images/warfacebezss.png",
+    image: "../images/warfacebezss.png",
     name: "Аккаунты для ГАЧИ-МУЧИ",
   },
 ];
