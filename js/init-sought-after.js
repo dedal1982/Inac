0284f247a2655be802500427a0cde77d4e5864d2
Al Dm <d44.a1@yandex.ru>
2025-03-04 20:44:25 +0300

    1

--- js/init-sought-after.js
+++ js/init-sought-after.js
@@ -44,9 +44,33 @@ initSoughtAfter.forEach(function (item) {
 const moreButton = document.querySelector(".in-demand__more");
 const backButton = document.querySelector(".in-demand__back");
 const pageGrid = document.querySelector(".in-demand__grid-pre");
+const elementsFull = document.querySelector(".ac-elements-full");
+
+const initSoughtAfterFull = [
+  {
+    image: "../../images/warfacebezss.png",
+    name: "1",
+  },
+  {
+    image: "../../images/warfacebezss.png",
+    name: "2",
+  },
+  {
+    image: "../../images/warfacebezss.png",
+    name: "3",
+  },
+  {
+    image: "../../images/warfacebezss.png",
+    name: "3",
+  },
+];
 
 if (initSoughtAfterFull.length > 0) {
   moreButton.classList.add("active");
+  initSoughtAfterFull.forEach(function (item) {
+    const elementCreate = createCard(item);
+    elementsFull.append(elementCreate);
+  });
 } else {
   moreButton.classList.remove("active");
 }
@@ -56,6 +80,7 @@ if (moreButton) {
     pageGrid.classList.add("in-demand__grid-pre-full");
     backButton.classList.add("active");
     moreButton.classList.remove("active");
+    elementsFull.classList.add("active");
   });
 }
 if (backButton) {
@@ -63,5 +88,6 @@ if (backButton) {
     pageGrid.classList.remove("in-demand__grid-pre-full");
     backButton.classList.remove("active");
     moreButton.classList.add("active");
+    elementsFull.classList.remove("active");
   });
 }
